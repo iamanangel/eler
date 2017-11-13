0e4dc98670bceca5077f3d90d1a43b4f027fb97a
xiewenxia <1978599765@qq.com>
2017-11-13 14:27:48 +0800

    second3 commit

--- public/backEnd/js/productManage.js
+++ public/backEnd/js/productManage.js
@@ -28,8 +28,8 @@ $(function () {
                     totalPages: Math.ceil(data.total / data.size),
                     /*点击页面事件*/
                     onPageClicked: function (event, originalEvent, type, page) {
-                      /*改变当前页再渲染 page当前点击的按钮的页面*/
-                      getSecondData(page);
+                        /*改变当前页再渲染 page当前点击的按钮的页面*/
+                        getSecondData(page);
                     }
                 });
             }
@@ -37,6 +37,124 @@ $(function () {
     }
 
     getProductManage();
+
+    var picList = [];
+    var upload = function () {
+        // 下面的id是type=file类型的input的id
+        $("#pic").fileupload({
+          // 找到上传图片的接口
+          url: "/product/addProductPic",
+          done: function (e,data) {
+            // console.log(data);
+            $('.fileupload').append('<img width="50" src="'+ data.result.picAddr +'" alt="">');
+            // console.log(data.result);
+            picList.push(data.result);
+            // console.log(picList);
+            
+          }
+        })
+    }
+
+
+      upload();
+
+    // 校验功能
+    $('#productform').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            // 字段名是name属性的值
+            proName: {
+                validators: {
+                    notEmpty: {
+                        message: '商品名称不能为空'
+                    }
+                }
+            },
+            proDesc: {
+                validators: {
+                    notEmpty: {
+                        message: '商品描述不能为空'
+                    }
+                }
+            },
+            num: {
+                validators: {
+                    notEmpty: {
+                        message: '库存不能为空'
+                    }
+                }
+            },
+            price: {
+                validators: {
+                    notEmpty: {
+                        message: '价格不能为空'
+                    }
+                }
+            },
+            oldPrice: {
+                validators: {
+                    notEmpty: {
+                        message: '商品原价不能为空'
+                    }
+                }
+            },
+            size: {
+                validators: {
+                    notEmpty: {
+                        message: '商品尺码不能为空'
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function (e) {
+        // Prevent form submission
+        e.preventDefault();
+        // Get the form instance
+        var $form = $(e.target);
+
+        var data = $form.serialize();
+
+        // console.log(data);
+        // proName=123
+        // &proDesc=123
+        // &num=123
+        // &price=123
+        // &oldPrice=123
+        // &size=123
+        // 除了data中的数据，还需要id，brandId,PIC图片(从哪取到pic路径)
+        $.each(picList, function (i, item) {
+            // console.log(i,item);
+            data += '&picName' +(i+1) + '=' + item.picName + '&picAddr' +(i+1) + '=' + item.picAddr;
+            
+        })
+        // console.log(data);
+
+        // 现在图片中已经加入了图片路径
+        // 还需要加入brandId
+        data+='&brandId=4';
+        // console.log(data);
+
+        // 数据已经·准备好，开始发送id
+        $.ajax({
+            type: 'post',
+            url: '/product/addProduct',
+            data: data,
+            dataType: 'json',
+            success: function (data) {
+                // console.log(data);
+                $('#productModel').modal('hide');
+                getProductManage();
+            }
+        })
+        
+
+    });
+
+
 
     
 })
